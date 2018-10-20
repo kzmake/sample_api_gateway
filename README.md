12130dee7802b4a83f71260670e24f63078876b4
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2018-10-20 18:05:52 +0900

    :recycle: hostのマッチング機能を使ってみる

--- README.md
+++ README.md
@@ -1,9 +1,38 @@
 # ExApiGateway
 
+Plugの機能をためしてみる。path と host でマッチングして分岐できるっぽい。
+
+## サーバー起動
+
 ```sh
 mix server
 ```
 
+## リクエスト例
+
+### ping
 ```sh
-curl -H "host: jp-east-1.computing.api.nifcloud.com" "http://localhost:4000/computing/?Action=XXXX
+$ curl "http://localhost:4000/ping"
+Pong localhost
+
+$ curl -H "host: jp-east-1.computing.api.nifcloud.com" "http://localhost:4000/ping"
+Pong computing
+
+$ curl -H "host: api.github.com" "http://localhost:4000/ping"
+Pong github
+
+$ curl -H "host: example.com" "http://localhost:4000/ping"
+No matching host, example.com⏎
+```
+
+### nifcloud
+```sh
+$ curl -H "host: jp-east-1.computing.api.nifcloud.com" "http://localhost:4000/api/?Action=XXXX"
+<Response><Errors><Error><Code>Client.AuthFailureRequired.AccessKey</Code><Message>No access key was provided.</Message></Error></Errors><RequestID>xxxx</RequestID></Response>⏎
+```
+
+### github
+```sh
+$ curl -H "host: api.github.com" "http://localhost:4000/users"
+[{"login":...}]
 ```
--- lib/router.ex
+++ lib/router.ex
@@ -7,19 +7,52 @@ defmodule ExApiGateway.Router do
   plug :match
   plug :dispatch
 
-  get "/hello" do
-    "Hello, world!"
+  get "/ping", host: "jp-east-1.computing." do
+    "Pong computing"
     |> just
     |> response
   end
 
-  get "/computing/" do
+  get "/ping", host: "api.github.com" do
+    "Pong github"
+    |> just
+    |> response
+  end
+
+  get "/ping", host: "localhost" do
+    "Pong localhost"
+    |> just
+    |> response
+  end
+
+  get "/ping" do
+    "No matching host, #{conn.host}"
+    |> just
+    |> response
+  end
+
+  match "/api/", host: "jp-east-1.computing." do
     host = conn.host
+    path = conn.request_path
     query_string = conn.query_string
+    method = conn.method
 
-    IO.inspect("https://#{host}/api/?#{query_string}")
+    IO.inspect(%{method: method, url: "https://#{host}#{path}", body: "#{query_string}"})
 
-    %{method: :post, url: "https://#{host}/api/", body: "#{query_string}"}
+    %{method: method, url: "https://#{host}#{path}", body: "#{query_string}"}
+    |> request
+    |> response
+  end
+
+  match "/*glob", host: "api.github.com" do
+    host = conn.host
+    path = conn.request_path
+    query_string = conn.query_string
+    method = conn.method
+
+    IO.inspect("https://#{host}#{path}")
+
+    %{method: method, url: "https://#{host}#{path}", body: "#{query_string}"}
     |> request
     |> response
   end
